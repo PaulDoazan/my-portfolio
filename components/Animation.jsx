489a0b330f8f15d1252e5200205f703d7f599247
pdoazan <paul.doazan@gmail.com>
2023-01-05 10:22:23 +0100

    Fix performance issue of stage

--- components/Animation.jsx
+++ components/Animation.jsx
@@ -8,24 +8,6 @@ export default function Animation() {
     const canvasRef = useRef(null);
 
     const onScriptLoaded = () => {
-        // const createjs = window.createjs;
-        // const stage = new createjs.Stage(canvasRef.current);
-        // createjs.Touch.enable(stage);
-
-        // // start the tick and point it at the window so we can do some work before updating the stage:
-        // createjs.Ticker.timingMode = createjs.Ticker.RAF;
-        // if(!createjs.Ticker.hasEventListener("tick")) createjs.Ticker.on("tick", tick);
-        
-        // console.log('onScriptLoaded');
-        // root(stage);
-
-        // function tick() {
-        //     stage.update();
-        // }
-    }
-
-    useEffect(() => {
-        if(window.createjs === undefined) return;
         const createjs = window.createjs;
         const stage = new createjs.Stage(canvasRef.current);
         createjs.Touch.enable(stage);
@@ -39,6 +21,29 @@ export default function Animation() {
         function tick() {
             stage.update();
         }
+    }
+
+    useEffect(() => {
+        if(window.createjs === undefined) return;
+        const createjs = window.createjs;
+        let stage = new createjs.Stage(canvasRef.current);
+        let tickHandler;
+        createjs.Touch.enable(stage);
+
+        // start the tick and point it at the window so we can do some work before updating the stage:
+        createjs.Ticker.timingMode = createjs.Ticker.RAF;
+        if(!createjs.Ticker.hasEventListener("tick")) tickHandler = createjs.Ticker.addEventListener("tick", tick);
+        
+        root(stage);
+
+        function tick() {
+            if(stage !== null) stage.update();
+        }
+
+        return function cleanup() {
+            createjs.Ticker.removeAllEventListeners()
+            stage = null;
+          };
     }, [])
 
     return (
