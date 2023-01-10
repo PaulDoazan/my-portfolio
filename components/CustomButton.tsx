5bbcc3109d794b3a2760b98089ebfdca68355fe8
pdoazan <paul.doazan@gmail.com>
2023-01-10 23:52:01 +0100

    Achieve Bixie animation

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,7 +3,7 @@ import utilStyles from '../styles/utils.module.css'
 import { selectPostState, setPostState } from "../store/postSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from 'next/router'
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, createElement  } from 'react';
 import explosionStyles from '../styles/explosion.module.css'
 
 interface ButtonProps {
@@ -18,44 +18,31 @@ export default function CustomButton({btnContent} : ButtonProps) {
           dispatch(setPostState(btnContent));
     }
 
-    useEffect(() => {
-      postState === btnContent ? explode() : null;
-    }, [postState])
+    // useEffect(() => {
+    //   postState === btnContent ? explode() : null;
+    // }, [postState])
     
 
-    const explosionContainer = useRef(null);
+    // const containerRef = useRef(null);
 
-    const explode = () => {
-      let particles = 15;
-      console.log(explosionContainer.current);
-      
-      // explosion container and its reference to be able to delete it on animation end
-      // explosion = $('<div class="explosion"></div>');
-    
-      // position the container to be centered on click
-      // explosion.css('left', x - explosion.width() / 2);
-      // explosion.css('top', y - explosion.height() / 2);
-    
-      // for (var i = 0; i < particles; i++) {
-      //   // positioning x,y of the particle on the circle (little randomized radius)
-      //   var x = (explosion.width() / 2) + rand(80, 150) * Math.cos(2 * Math.PI * i / rand(particles - 10, particles + 10)),
-      //     y = (explosion.height() / 2) + rand(80, 150) * Math.sin(2 * Math.PI * i / rand(particles - 10, particles + 10)),
-      //     color = rand(0, 255) + ', ' + rand(0, 255) + ', ' + rand(0, 255), // randomize the color rgb
-      //       // particle element creation (could be anything other than div)
-      //     elm = $('<div class="particle" style="' +
-      //       'background-color: rgb(' + color + ') ;' +
-      //       'top: ' + y + 'px; ' +
-      //       'left: ' + x + 'px"></div>');
-    
-      //   if (i == 0) { // no need to add the listener on all generated elements
-      //     // css3 animation end detection
-      //     elm.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-      //       explosion.remove(); // remove this explosion container when animation ended
-      //     });
-      //   }
-      //   explosion.append(elm);
-      // }
-    }
+    // const explode = () => {
+    //   let particles = 15;
+
+    //   for (var i = 0; i < particles; i++) {
+    //       let p = document.createElement('div');
+    //       p.className = 'particle';
+    //       p.style.left = `${rand(0, 100)}%`;
+    //       p.style.top = '50%';
+    //       if(containerRef.current) containerRef.current.appendChild(p);
+
+    //       p.addEventListener('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+    //         console.log('end');
+            
+    //         if(containerRef.current) containerRef.current.removeChild(p); // remove this explosion container when animation ended
+    //       });
+              
+    //     }
+    // }
     
     // get random number between min and max value
     function rand(min : number, max : number) {
@@ -65,7 +52,7 @@ export default function CustomButton({btnContent} : ButtonProps) {
   return (
     <>
         <Link data-id="customBtn" className={`${explosionStyles.linkContainer}`} onClick={handleClick} href={`/posts/${btnContent.toLowerCase()}`}>
-            <span ref={explosionContainer} className={`${explosionStyles.explosion}`}></span>
+            {/* <span ref={containerRef} className={`${explosionStyles.explosion}`}></span> */}
             <span className={postState === btnContent ? utilStyles.fullYellowLink : utilStyles.thinYellowLink}>{btnContent}</span>
         </Link>
     </>
