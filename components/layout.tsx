1067d30deb451ed107a3319806a5536c8a5772b5
pdoazan <paul.doazan@gmail.com>
2023-01-26 09:28:43 +0100

    Add Vercel Analytics

--- components/layout.tsx
+++ components/layout.tsx
@@ -70,12 +70,3 @@ export default function Layout({
     </div>
   )
 }
-
-// export const getStaticProps: GetStaticProps = async () => {
-//   const allPostsData = getSortedPostsData()
-//   return {
-//     props: {
-//       allPostsData
-//     }
-//   }
-// }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
 import { wrapper } from "../store/store";
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </> 
+  
 }
 
 export default wrapper.withRedux(App);
